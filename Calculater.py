a77889df7b801bd7df4b057db8d3019ac5407d45
Fiza Arzoo Khalid <shaizakanwal025@gmail.com>
2025-04-29 12:30:47 +0500

    Add files via upload

---
+++ Calculater.py
@@ -0,0 +1,10 @@
+first_number =int(input("Enter first number:"))
+second_number =int(input("Enter second number:"))
+add = first_number + second_number
+sub = first_number - second_number
+multi= first_number * second_number
+divi = first_number // second_number
+print("Addiction:",add)
+print("Subtraction:",sub)
+print("Multiplication:",multi)
+print("Division:",divi)
